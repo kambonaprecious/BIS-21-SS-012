477b0601da70d07e428d0f280098d9e7acc6d911
kambonaprecious <kambonaprecious@gmail.com>
2025-08-06 09:02:40 +0300

    1

---
+++ 1.js
@@ -0,0 +1,21 @@
+//number
+let number = 12;
+console.log(typeof number);
+
+//string
+let word = "yeah man";
+console.log(typeof word);
+
+//boolean
+let is_This_JS = true;
+console.log(typeof is_This_JS);
+
+//object (array)
+let value1 = [1, 2, 3, 4];
+console.log(typeof value1);
+
+//undefined
+let num1 = undefined;
+console.log(typeof num1);
+
+
